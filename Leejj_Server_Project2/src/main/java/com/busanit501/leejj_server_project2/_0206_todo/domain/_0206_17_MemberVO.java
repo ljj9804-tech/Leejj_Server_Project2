eda92ca7bd0f098a99086cc9efba3d12330acf3a
leejinju <ljj9804@gmail.com>
2026-02-06 18:49:43 +0900

    20260206_수업_업데이트

---
+++ Leejj_Server_Project2/src/main/java/com/busanit501/leejj_server_project2/_0206_todo/domain/_0206_17_MemberVO.java
@@ -0,0 +1,15 @@
+package com.busanit501.leejj_server_project2._0206_todo.domain;
+
+import lombok.*;
+
+@Getter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class _0206_17_MemberVO {
+    // 데이터베이스의 컬럼과 동일하게 작업.
+    private String mid;
+    private String mpw;
+    private String mname;
+}
